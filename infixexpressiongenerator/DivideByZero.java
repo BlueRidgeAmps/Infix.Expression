a884852ac7faaddcd0785b446b9b10e7ea074d3e
Jonathan Pearson <33793038+BlueRidgeAmps@users.noreply.github.com>
2017-12-27 08:59:44 -0500

    Update DivideByZero.java

--- infixexpressiongenerator/DivideByZero.java
+++ infixexpressiongenerator/DivideByZero.java
@@ -1,16 +1,3 @@
-package infixexpressiongenerator;
-
-/**
- * Course: CMSC 350-6380 Data Structures and Analysis
- * Project 2
- * 
- * File: DivideByZero.java 
- * Author: Jonathan Pearson 
- * Date: February 4, 2017
- * Purpose: This class creates a custom exception to be thrown if the user input
- *          results in a division by zero case.
- * 
- */
 public class DivideByZero extends Exception {
 
     public DivideByZero() {
